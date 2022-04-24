d8fd4981cd70f3afe7a88e37fe9aa0e6eae10cba
yapyeeqiang <yapyeeqiang@gmail.com>
2022-04-25 00:01:37 +0800

    🍱 Add social icons and logos

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import GitHubIcon from './GitHubIcon.vue'
+import InstagramIcon from './InstagramIcon.vue'
+import LinkedInIcon from './LinkedInIcon.vue'
+import TwitterIcon from './TwitterIcon.vue'
+
+export { GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon }
