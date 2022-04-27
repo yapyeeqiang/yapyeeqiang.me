2b44201970ea3b5702a5d54e636961aecd5c6c2d
yapyeeqiang <yapyeeqiang@gmail.com>
2022-04-28 03:08:51 +0800

    ⚡️ Add env types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_GITHUB_ACCESS_TOKEN: string
+    readonly VITE_NOTION_TOKEN: string
+    readonly VITE_NOTION_DATABASE_ID: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
