1e50238544dcf13c4fb3e2f2bc71565286d5a13d
yapyeeqiang <yapyeeqiang@gmail.com>
2022-04-30 18:33:58 +0800

    ✨ Add express server for notion api

--- server/app.js
+++ server/app.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', async (req, res) => {
+    res.send("Hello! This is Yee Qiang's server.")
+})
+
 app.get('/database', async (req, res) => {
     const query = await notion.databases.query({ database_id: process.env.VITE_NOTION_DATABASE_ID })
 
@@ -24,6 +28,15 @@ app.get('/pages/:id', async (req, res) => {
     })
 
     res.json(contents)
+})
+
+app.get('/user/:id', async (req, res) => {
+    const { id } = req.params
+    const user = await notion.users.retrieve({
+        user_id: id,
+    })
+
+    res.json(user)
 })
 
 app.listen(8080, () => {
