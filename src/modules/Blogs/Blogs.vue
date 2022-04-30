ef6766ec4e0cfe48028d06e0893d0463535f46e2
yapyeeqiang <yapyeeqiang@gmail.com>
2022-04-30 18:33:34 +0800

    ✨ Add coming soon logic to blog and community page

--- src/modules/Blogs/Blogs.vue
+++ src/modules/Blogs/Blogs.vue
@@ -1,27 +1,53 @@
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue'
-// import notion from '@/api/notion'
-// import { onMounted } from 'vue'
+import notion from '@/api/notion'
+import { onMounted, ref } from 'vue'
+import type { BlogPreview } from '@/interfaces/notion'
+import { formatDate } from '@/composables/useDate'
+import { useLoading } from '@/composables/useLoading'
+import Loading from '@/components/Loading.vue'
+import BlogCard from './components/BlogCard.vue'
 
-// onMounted(async () => {
-//     const pages = await notion.getDatabase()
+const isComingSoon = ref(true)
+const blogs = ref<BlogPreview[]>([])
 
-//     pages.forEach(async (page: any) => {
-//         const pageContent = await notion.getPageContent(page.id)
+const { isLoading, startLoading, stopLoading } = useLoading()
 
-//         console.log(pageContent)
-//     })
-// })
+onMounted(async () => {
+    if (isComingSoon.value) return
+
+    startLoading()
+
+    blogs.value = []
+    const pages = await notion.getDatabase()
+
+    for (let page of pages) {
+        const authorID = page.created_by.id
+
+        const user = await notion.getPageAuthor(authorID)
+
+        blogs.value.push({
+            id: page.id,
+            title: page.properties.Name.title[0].plain_text,
+            cover: page.cover?.external.url,
+            category: page.properties.Category.multi_select[0].name,
+            description: page.properties.Description.rich_text[0].plain_text,
+            author: {
+                name: user.name,
+                imageURL: user.avatar_url,
+            },
+            createdAt: formatDate(page.created_time),
+        })
+    }
+
+    stopLoading()
+})
 </script>
 
 <template>
     <MainLayout>
         <div class="max-w-5xl mx-auto">
-            <div v-if="false" class="flex flex-col items-start">
-                <h1 class="text-4xl font-semibold">Blogs</h1>
-            </div>
-
-            <div v-else class="flex flex-col items-center justify-center py-32 space-y-10">
+            <div v-if="isComingSoon" class="flex flex-col items-center justify-center py-32 space-y-10">
                 <div class="flex flex-col items-center space-y-4">
                     <h1 class="text-4xl font-semibold">Coming soon.</h1>
                     <p class="text-gray-400 font-light italic text-center">Stay tuned for this amazing blogs section!</p>
@@ -30,6 +56,18 @@ import MainLayout from '@/layouts/MainLayout.vue'
                 <div class="animate-pulse hover:animate-none">
                     <router-link class="hover:text-zinc-600 dark:text-gray-50" to="/">Go back home &rarr;</router-link>
                 </div>
+            </div>
+
+            <div v-else class="flex flex-col items-start space-y-10">
+                <h1 class="text-4xl font-semibold">Blogs</h1>
+
+                <Loading :is-loading="isLoading">
+                    <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                        <div v-for="blog in blogs" :key="blog.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                            <BlogCard :blog="blog" />
+                        </div>
+                    </div>
+                </Loading>
             </div>
         </div>
     </MainLayout>
--- src/modules/Community/Community.vue
+++ src/modules/Community/Community.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue'
+import { ref } from 'vue'
+
+const isComingSoon = ref(true)
 </script>
 
 <template>
     <MainLayout>
         <div class="max-w-5xl mx-auto">
-            <div v-if="false" class="flex flex-col items-start">
-                <h1 class="text-4xl font-semibold">Community</h1>
-            </div>
-
-            <div v-else class="flex flex-col items-center justify-center py-32 space-y-10">
+            <div v-if="isComingSoon" class="flex flex-col items-center justify-center py-32 space-y-10">
                 <div class="flex flex-col items-center space-y-4">
                     <h1 class="text-4xl font-semibold">Coming soon.</h1>
                     <p class="text-gray-400 font-light italic text-center">Stay tuned for some great news from the community!</p>
@@ -18,6 +17,12 @@ import MainLayout from '@/layouts/MainLayout.vue'
                 <div class="animate-pulse hover:animate-none">
                     <router-link class="hover:text-zinc-600 dark:text-gray-50" to="/">Go back home &rarr;</router-link>
                 </div>
+            </div>
+
+            <div v-else class="flex flex-col items-start">
+                <h1 class="text-4xl font-semibold">Community</h1>
+
+                <iframe src="https://discord.com/widget?id=840260570737541171&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             </div>
         </div>
     </MainLayout>
